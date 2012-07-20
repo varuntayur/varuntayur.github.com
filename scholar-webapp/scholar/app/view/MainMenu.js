80308c3f1684a409136e346c70735aa352eb7d94
Varun Tayur <varuntayur@gmail.com>
2012-07-20 12:41:54 +0530

    Adding site template options

--- scholar-webapp/scholar/app/view/MainMenu.js
+++ scholar-webapp/scholar/app/view/MainMenu.js
@@ -42,231 +42,242 @@ Ext.define('scholar.view.MainMenu', {
 		'scholar.view.detail.student.performance.Report',
 		'scholar.view.detail.student.attendance.Manager'],
 
-	items : [ {
-		mainItem : 0,
-		items : [ {
-			iconCls: 'x-icon-student',
-			title : 'Student',
-			tabTip : 'Wear the Student hat',
-			border : false,
-			xtype : 'studentDashboard'
-		}, {
-			title : 'Lookup',
-			tabTip : 'Lookup student detail',
-			iconCls: 'x-icon-search',
-			xtype : 'studentSearchDetail',
-			margin : '10',
-			height : null
-		}, {
-			title : 'Attendance',
-			tabTip : 'Attendance',
-			iconCls: 'x-icon-attendance',
-			xtype : 'attendanceManager',
-			margin : '10',
-			height : null
-		}, {
-			title : 'Performance',
-			tabTip : 'Student search',
-			iconCls: 'x-icon-grades',
-			xtype : 'performanceReport',
-			margin : '10',
-			height : null
-		} ]
-	}, {
-		mainItem : 0,
-		items : [ {
-			title : 'Staff',
-			iconCls: 'x-icon-staff',
-			tabTip : 'Wear the Staff hat',
-			border : false,
-				xtype : 'staffDashboard'
-		}, {
-			title : 'Lookup',
-			tabTip : 'Lookup staff information',
-			iconCls: 'x-icon-search',
-			xtype : 'staffLookup',
-			margin : '10',
-			height : null
-		}, {
-			title : 'Attendance',
-			tabTip : 'Attendance',
-			iconCls: 'x-icon-attendance',
-			xtype : 'staffAttendanceManager',
-			margin : '10',
-			height : null
-		}, {
-			title : 'TimeTable',
-			tabTip : 'Manage Timetable',
-			iconCls:'x-icon-timetable',
-			xtype : 'timeTable',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'Leave Absence',
-			tabTip : 'Manage/Apply Leaves',
-			iconCls:'x-icon-leave',
-			xtype : 'leaveManager',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'Events/Memo',
-			tabTip : 'Issue Memos and publish Event Calendars',
-			iconCls:'x-icon-events',
-			xtype : 'eventManager',
-			margin : '10',
-			height : null
-		}
-		]
-	},	
-	{
-		mainItem : 0,
-		items : [ {
-			title : 'Configuration',
-			tabTip : 'Be the administrator',
-			iconCls: 'x-icon-administrator',
-			border : false,
-				xtype : 'administrationDashboard'
-		},
-		{
-			title : 'Admissions',
-			tabTip : 'Admissions',
-			iconCls: 'x-icon-admission',
-			xtype : 'admissionManager',
-			margin : '10',
-			height : null
-		},	
-		{
-			title : 'Course',
-			tabTip : 'Course Management',
-			iconCls: 'x-icon-course',
-			xtype : 'courseManager',
-			margin : '10',
-			height : null
-		},		
-		{
-			title : 'Batch',
-			tabTip : 'Batch Management',
-			iconCls: 'x-icon-batch',
-			xtype : 'batchManager',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'Examination',
-			tabTip : 'Examination Management',
-			iconCls: 'x-icon-exam',
-			xtype : 'examManager',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'Inventory',
-			tabTip : 'Inventory',
-			iconCls: 'x-icon-inventory',
-			xtype : 'inventoryManager',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'User/Role',
-			tabTip : 'Manager User Roles',
-			iconCls:'x-icon-userrole',
-			xtype : 'userRoleManager',
-			margin : '10',
-			height : null
-		}
-		]
-	},	
-	{
-		mainItem : 0,
-		items : [
-		           {
-						title : 'Finance',
-						tabTip : 'Manage Finances',
+	items : [
+			{
+				mainItem : 0,
+				items : [
+					{
+						title : 'Administrator',
+						tabTip : 'Be the administrator',
+						iconCls: 'x-icon-administrator',
 						border : false,
-						xtype : 'otherDashboard'
+						xtype : 'administrationDashboard'
 					},
 					{
-						title : 'Payroll',
-						tabTip : 'Payroll',
-						iconCls: 'x-icon-payroll',
-						xtype : 'payrollManager',
+						title : 'Admissions',
+						tabTip : 'Admissions',
+						iconCls: 'x-icon-admission',
+						xtype : 'admissionManager',
+						margin : '10',
+						height : null
+					},	
+					{
+						title : 'Batch',
+						tabTip : 'Batch Management',
+						iconCls: 'x-icon-batch',
+						xtype : 'batchManager',
 						margin : '10',
 						height : null
 					},
 					{
-						title : 'Fee Collection',
-						tabTip : 'Fee Collection',
-						iconCls: 'x-icon-fees',
-						xtype : 'feeCollectionManager',
+						title : 'Course',
+						tabTip : 'Course Management',
+						iconCls: 'x-icon-course',
+						xtype : 'courseManager',
+						margin : '10',
+						height : null
+					},		
+					{
+						title : 'Examination',
+						tabTip : 'Examination Management',
+						iconCls: 'x-icon-exam',
+						xtype : 'examManager',
+						margin : '10',
+						height : null
+					},
+					{
+						title : 'Inventory',
+						tabTip : 'Inventory',
+						iconCls: 'x-icon-inventory',
+						xtype : 'inventoryManager',
+						margin : '10',
+						height : null
+					},
+					{
+						title : 'User/Role',
+						tabTip : 'Manager User Roles',
+						iconCls:'x-icon-userrole',
+						xtype : 'userRoleManager',
 						margin : '10',
 						height : null
 					}
-	             ]
-	},
-	{
-		mainItem : 0,
-		items : [ {
-			title : 'Library',
-			iconCls: 'x-icon-library',			
-			tabTip : 'Wear the Librarian hat',
-			border : false,
-				xtype : 'libraryDashboard'
-		},
-		{
-			title : 'Book Search',
-			tabTip : 'Book search',
-			xtype : 'libraryLookup',
-			iconCls: 'x-icon-search',
-			margin : '10',
-			height : null
-		},
-		{
-			title : 'OPAC',
-			tabTip : 'OPAC search',
-			xtype : 'opacLookup',
-			margin : '10',
-			height : null
-		}
-		]
-	},
-	{
-		mainItem : 0,
-		items : [ {
-					title : 'Transport',
-					tabTip : 'Manage Transport',
-					iconCls: 'x-icon-transport',
-					border : false,
-					xtype : 'parentDashboard'
-				  },
-				  {
-						title : 'Vehicle',
-						tabTip : 'Manage Vehicle fleet',
-						iconCls: 'x-icon-vehicle',
+				]
+			},
+	        {
+				mainItem : 0,
+				items : [
+					{
+						iconCls: 'x-icon-student',
+						title : 'Student',
+						tabTip : 'Wear the Student hat',
 						border : false,
-						xtype : 'vehicleLookup'
-				  },
-				  {
-						title : 'Route',
-						tabTip : 'Manage Vehicle routes',
-						iconCls: 'x-icon-route',
+						xtype : 'studentDashboard'
+					}, {
+						title : 'Lookup',
+						tabTip : 'Lookup student detail',
+						iconCls: 'x-icon-search',
+						xtype : 'studentSearchDetail',
+						margin : '10',
+						height : null
+					}, {
+						title : 'Attendance',
+						tabTip : 'Attendance',
+						iconCls: 'x-icon-attendance',
+						xtype : 'attendanceManager',
+						margin : '10',
+						height : null
+					}, {
+						title : 'Performance',
+						tabTip : 'Student search',
+						iconCls: 'x-icon-grades',
+						xtype : 'performanceReport',
+						margin : '10',
+						height : null
+					}
+				]
+	         },
+	         {
+				mainItem : 0,
+				items : [
+					{
+						title : 'Staff',
+						iconCls: 'x-icon-staff',
+						tabTip : 'Wear the Staff hat',
 						border : false,
-						xtype : 'routeLookup'
-				  }			  
-		        ]
-	},
-	{
-		mainItem : 0,
-		items : [ {
-					title : 'Parent',
-					tabTip : 'Parental View',
-					iconCls: 'x-icon-userrole',
-					border : false,
-					xtype : 'parentDashboard'
-				  }				  
-		        ]
-	}
+							xtype : 'staffDashboard'
+					}, {
+						title : 'Lookup',
+						tabTip : 'Lookup staff information',
+						iconCls: 'x-icon-search',
+						xtype : 'staffLookup',
+						margin : '10',
+						height : null
+					}, {
+						title : 'Attendance',
+						tabTip : 'Attendance',
+						iconCls: 'x-icon-attendance',
+						xtype : 'staffAttendanceManager',
+						margin : '10',
+						height : null
+					}, {
+						title : 'TimeTable',
+						tabTip : 'Manage Timetable',
+						iconCls:'x-icon-timetable',
+						xtype : 'timeTable',
+						margin : '10',
+						height : null
+					},
+					{
+						title : 'Leave Absence',
+						tabTip : 'Manage/Apply Leaves',
+						iconCls:'x-icon-leave',
+						xtype : 'leaveManager',
+						margin : '10',
+						height : null
+					},
+					{
+						title : 'Events/Memo',
+						tabTip : 'Issue Memos and publish Event Calendars',
+						iconCls:'x-icon-events',
+						xtype : 'eventManager',
+						margin : '10',
+						height : null
+					}
+				]
+	         },	
+	         {
+					mainItem : 0,
+					items : [ 
+					          {
+								title : 'Parent',
+								tabTip : 'Parental View',
+								iconCls: 'x-icon-userrole',
+								border : false,
+								xtype : 'parentDashboard'
+							  }				  
+					 ]
+		     },
+		     {
+					mainItem : 0,
+					items : [ 
+						{
+							title : 'Library',
+							iconCls: 'x-icon-library',			
+							tabTip : 'Wear the Librarian hat',
+							border : false,
+								xtype : 'libraryDashboard'
+						},
+						{
+							title : 'Book Search',
+							tabTip : 'Book search',
+							xtype : 'libraryLookup',
+							iconCls: 'x-icon-search',
+							margin : '10',
+							height : null
+						},
+						{
+							title : 'OPAC',
+							tabTip : 'OPAC search',
+							xtype : 'opacLookup',
+							margin : '10',
+							height : null
+						}
+					]
+		     },
+	         {
+				mainItem : 0,
+				items : [
+				           {
+								title : 'Finance',
+								tabTip : 'Manage Finances',
+								border : false,
+								xtype : 'otherDashboard'
+							},
+							{
+								title : 'Payroll',
+								tabTip : 'Payroll',
+								iconCls: 'x-icon-payroll',
+								xtype : 'payrollManager',
+								margin : '10',
+								height : null
+							},
+							{
+								title : 'Fee Collection',
+								tabTip : 'Fee Collection',
+								iconCls: 'x-icon-fees',
+								xtype : 'feeCollectionManager',
+								margin : '10',
+								height : null
+							}
+			     ]
+	         },
+	         
+	         {
+				mainItem : 0,
+				items : [
+				         {
+							title : 'Transport',
+							tabTip : 'Manage Transport',
+							iconCls: 'x-icon-transport',
+							border : false,
+							xtype : 'parentDashboard'
+						  },
+						  {
+								title : 'Vehicle',
+								tabTip : 'Manage Vehicle fleet',
+								iconCls: 'x-icon-vehicle',
+								border : false,
+								xtype : 'vehicleLookup'
+						  },
+						  {
+								title : 'Route',
+								tabTip : 'Manage Vehicle routes',
+								iconCls: 'x-icon-route',
+								border : false,
+								xtype : 'routeLookup'
+						  }			  
+				 ]
+	         }
+	         
 	]
 });
