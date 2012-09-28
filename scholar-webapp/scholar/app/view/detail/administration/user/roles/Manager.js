0b2b731046ec0f390e6eef68d1fce8e77277342e
varuntayur <varun.tayur@gmail.com>
2012-09-28 17:04:53 +0530

    roles screen prototype fixed

--- scholar-webapp/scholar/app/view/detail/administration/user/roles/Manager.js
+++ scholar-webapp/scholar/app/view/detail/administration/user/roles/Manager.js
@@ -1,24 +1,25 @@
 Ext.define('scholar.view.detail.administration.user.roles.Manager', {
-	extend : 'Ext.panel.Panel',
+	extend : 'Ext.form.Panel',
 	alias: 'widget.roleManager',
 	requires:['scholar.view.detail.administration.user.roles.RoleGrid',
 	          'scholar.view.detail.administration.user.roles.PermissionsGrid'],
-	layout:'column',
+	//layout:'column',
+	layout: {
+	      type: 'border'
+	},
     autoScroll:true,
+    defaults: {
+        split: true
+    },
     items: [
             {
-		        columnWidth: 1/2,
-		        baseCls:'x-plain',
-		        bodyStyle:'padding:5px 0 5px 5px',
-		        items:[{xtype:'roleGrid'}]
+		        flex: 2,
+            	xtype:'roleGrid',
+           		region:'center'
 		    },
 		    {
-		        columnWidth: 1/2,
-		        baseCls:'x-plain',
-		        bodyStyle:'padding:5px 0 5px 5px',
-		        items:[{
-		        	xtype:'permissionsGrid'
-		        }]
+		    	xtype:'permissionsGrid',
+		    	region: 'south'
 		    }
 		   ]	
 });
