3825a2da507e3b6b7c764d45f3c168c3d724b8aa
yatisolutions <yatisolutions@gmail.com>
2012-10-15 16:51:57 +0530

    library mvc fixes 2

--- scholar-webapp/scholar/app/view/library/lookup/Lookup.js
+++ scholar-webapp/scholar/app/view/library/lookup/Lookup.js
@@ -3,9 +3,23 @@ Ext.define('scholar.view.library.lookup.Lookup', {
 	requires : [ 'scholar.view.library.lookup.Search',
 	             'scholar.view.library.lookup.Detail'],
 	alias: 'widget.libraryLookup',
-	layout:'fit',
+	title:'Lookup',
+	frame : true,
+	bodyPadding : 5,
+	fieldDefaults : {
+		labelAlign : 'left',
+		msgTarget : 'side'
+	},
+	layout : {
+		 type : 'border',
+		 padding : 5
+	 },
+	 defaults : {
+		 split : true
+	 },
     items: [{
        			xtype: 'librarySearch',
+       			region:'center'
     		}]
 });
 
